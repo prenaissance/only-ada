8c0089e12ce596b10efdcb22ee4f19606b92a3e0
prenaissance <alexandru.andries@isa.utm.md>
2022-12-23 14:22:41 +0200

    Folder design, main page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className={"relative h-screen w-screen bg-[url(/backgrounds/ada.jpg)]"}>
+      <main className="text-black">
+        <div className="absolute flex flex-col items-center justify-center h-16 top-1/4">
+          <h1 className="text-4xl font-bold text-white">Welcome to Only Ada</h1>
+          <p className="">A place to find stuff you like from people you like. Become productive without burning out</p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
