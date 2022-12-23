81e49093dbd797781b62cba4af524883f4e1bd3e
Maria Colta <mashacolta@Marias-MacBook-Pro-2.local>
2022-12-23 21:41:07 +0200

    navbar

---
+++ src/pages/Courses.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@components/ui/Navbar";
+import CourseCard from "@features/courses/CourseCard";
+
+const Courses = () => {
+  return (
+    <div>
+      {/* <CourseCard /> */}
+      Co
+    </div>
+  );
+};
+
+export default Courses;
