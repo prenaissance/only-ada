3e4e6e40f28fd6297eaff3ea1bdc0ad6dd079833
Maria Colta <mashacolta@Marias-MacBook-Pro-2.local>
2022-12-23 19:27:27 +0200

    components library

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
-import Button from "@components/ui/Button";
+// import Button from "@components/ui/Button";
 import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const Home = () => {
   return (
@@ -7,19 +8,21 @@ const Home = () => {
       <main className="text-black">
         <div className="absolute flex flex-col items-center justify-center md:top-[40%] md:right-[15%] md:translate-x-0 top-6 left-1/2 -translate-x-1/2">
           <h1 className="text-4xl font-black">Welcome to Only Ada</h1>
-          <p className="max-w-xs">
+          <p className="max-w-xs pt-4">
             A place to find stuff you like from people you like. Become productive without burning out.
           </p>
-          <div>
+          <div className="pt-4 w-60 flex justify-between">
             <Link to="/courses">
-              <Button variant="outline" color="black">
+              {/* <Button variant="outlined" color="black">
                 Courses
-              </Button>
+              </Button> */}
+              <Button variant="outlined">Courses</Button>
             </Link>
             <Link to="/login">
-              <Button color="black" variant="outline">
+              {/* <Button color="black" variant="outline">
                 Login
-              </Button>
+              </Button> */}
+              <Button variant="outlined">Log in</Button>
             </Link>
           </div>
         </div>
