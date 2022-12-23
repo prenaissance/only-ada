54315c5f5a2e46a40cf6113c88fc819b7124a8a6
Maria Colta <mashacolta@Marias-MacBook-Pro-2.local>
2022-12-23 15:24:45 +0200

    added course card component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className={"relative h-screen w-screen bg-[url(/backgrounds/ada.jpg)]"}>
+    <div className={"relative h-screen w-screen bg-[url(/backgrounds/ada.png)] bg-no-repeat"}>
       <main className="text-black">
         <div className="absolute flex flex-col items-center justify-center h-16 top-1/4">
           <h1 className="text-4xl font-bold text-white">Welcome to Only Ada</h1>
