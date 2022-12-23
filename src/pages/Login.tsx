2f4de9b1ee64e4419cb5cb9da9b1b7d1aaab7a8e
prenaissance <alexandru.andries@isa.utm.md>
2022-12-23 22:11:25 +0200

    Form changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,7 +15,7 @@ const Login = () => {
       <div className="flex flex-col absolute items-center justify-center h-16 md:top-[40%] md:left-[15%] md:translate-x-0 top-6 left-1/2 -translate-x-1/2">
         <h1 className="text-4xl font-bold text-orange-900">Login to Only Ada</h1>
         <p className="text-white">Welcome or welcome back, anon</p>
-        {isRegistering ? <RegisterForm /> : <LoginForm isSecondStage={false} onMoveToRegister={handleMoveToRegister} />}
+        {isRegistering ? <RegisterForm /> : <LoginForm onMoveToRegister={handleMoveToRegister} />}
       </div>
     </div>
   );
